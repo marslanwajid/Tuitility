d43854f10107835501807da023f31ba64d626b46
marslanwajid <wajidmarslan@gmail.com>
2025-08-06 11:12:02 +0500

    header and footer added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/header.css"
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='header-heading'>Header</h1>
+    </div>
+  )
+}
+
+export default Header
