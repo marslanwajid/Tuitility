f15ea8c538785bdc5b700843400dddb7a86465c5
marslanwajid <wajidmarslan@gmail.com>
2025-08-06 10:59:46 +0500

    Router establised

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-    <center><h1 className='text-5xl font-bold text-red-500'>Hello World</h1></center>
+    <h1>Hello World</h1>
     </>
   )
 }
